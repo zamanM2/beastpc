d5e8c14ec584d72e91392bd329bf9229e1c2fc2e
Mohammad Badruzzaman <mohammad.badruzz@gmail.com>
2020-04-09 01:28:03 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Home from './Components/Home';
+import Cart from './Components/Cart';
+import Products from './Components/Products';
+import Reviews from './Components/Reviews';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello World
+      
+
+
+<Router>
+<div className = "nav">
+  <nav>
+    <ul>
+      
+        <Link to="/">Home</Link>
+      
+     
+        <Link to="/Store">Store</Link>
+     
+        <Link to="/Cart">Cart</Link>
+
+        <Link to="/Reviews">Reviews</Link>
+      
+      
+    </ul>
+  </nav>
+
+  
+  <Switch>
+  <Route path="/Cart">
+      <Cart/>
+    </Route>
+    <Route path="/Products">
+      <Products />
+    </Route>
+    <Route path="/Reviews">
+      <Reviews />
+    </Route>
+    <Route exact path="/">
+      <Home />
+    </Route>
+  </Switch>
+</div>
+</Router>
+</div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/Components/Cart.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+class Cart extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is our cart data</h1>
+            </div>
+        )
+    }
+}
+
+export default Cart
